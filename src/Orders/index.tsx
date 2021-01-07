c2349eef7a19db22645209596071e68bdc8e82ed
Wleydson Lemos <wleydsonredes@gmail.com>
2021-01-07 19:38:05 -0300

    listagem dos produtos

--- src/Orders/index.tsx
+++ src/Orders/index.tsx
@@ -1,10 +1,12 @@
 import './style.css';
 import StepsHeader from './StepsHeader'
+import ProductsList from './ProductsList'
 
 function Orders(){
     return(
        <div className="orders-container">
            <StepsHeader />
+           <ProductsList />
        </div>
     );
 }
